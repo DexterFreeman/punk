a5eb72af5a52c1414535a5429bc43520688ba137
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-02-09 14:49:21 +0000

    Added sass and beginning to create containers

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 
 import './App.scss';
+import punk from "./data/punk"
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nav-bar'>
+        Nav bar
+      </div>
+      <div className='main-content'>
+        <div className='main-content__card'>Hello :)</div>
+        <div className='main-content__card'>Hello :)</div>
+        <div className='main-content__card'>Hello :)</div>
+        <div className='main-content__card'>Hello :)</div>
+        <div className='main-content__card'>Hello :)</div>
+        <div className='main-content__card'>Hello :)</div>
+        <div className='main-content__card'>Hello :)</div>
+        <div className='main-content__card'>Hello :)</div>
+        <div className='main-content__card'>Hello :)</div>
+        <div className='main-content__card'>Hello :)</div>
+        <div className='main-content__card'>Hello :)</div>
+        <div className='main-content__card'>Hello :)</div>
+        <div className='main-content__card'>Hello :)</div>
+      </div>
     </div>
   );
 }
